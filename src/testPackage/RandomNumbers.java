9d91d5643e171db2b185e2496514c61a6ecae6e7
Manpreetsoodan2023 <manpreetsoodan85@gmail.com>
2023-04-11 14:13:31 -0300

    test commit

--- src/testPackage/RandomNumbers.java
+++ src/testPackage/RandomNumbers.java
@@ -1,28 +1,27 @@
 package testPackage;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomNumbers {
 
-	double[] numberArray = new double[10];
-	double numbers;
+	double[] numbers = new double[10];
+
 
 	Scanner sc = new Scanner(System.in);
 	
 	void populateNumbers() {
 
-		System.out.println("Enter any random numbers: ");
-		for (int j = 0 ; j<=10 ; j++) {
-         numbers = sc.nextDouble();
-		}
+		for (int i = 0; i < numbers.length; i++) {
+			
+			System.out.println("Enter number " + (i+1) + " : ") ;
 
-		for (int i = 0; i <= numberArray.length; i++) {
-
-			numberArray[i] = sc.nextDouble();
-
-			System.out.println(numberArray[i]);
+			numbers[i] = sc.nextDouble();
 
 		}
+			System.out.println(Arrays.toString(numbers));
+
+		
 	}
 
 }
